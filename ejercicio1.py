f9f154c1a2c416b6dfa3821564b598d02b307e19
Mariiangel15 <mariusuga72@gmail.com>
2025-05-19 10:37:30 -0500

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,13 @@
+print("ingrese la informacion del hermano 1")
+edad_h1 = int(input("Ingrese la edad del hermano 1")
+nombre_h1 = input ("ingrese el nombre del hermano 1")
+
+print ("ingrese la informacion del hermano 2")
+edad_h2 = int(input("ingrese la edad del hermano 2")
+nombre_h2 = print (" ingrese el nombre del nombre 2")
+
+if edad_h1 > edad_h2:
+    print ("el hermano mayor"), nombre_h1 ,"edad es", edad_h1)
+    else:
+
+    print ("el hermano mayor"), nombre_h2 ,"edad es", edad_h2)
---
+++ ejercicio2.py
@@ -0,0 +1,6 @@
+edad = int(input ("ingrese la edad"))
+if edad >0 and edad <10:
+    print ("es un niño")
+elif edad >=10 and edad <15:
+    print ("es un preadolecente")
+    elif 
